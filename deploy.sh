0d5b3f5897bf18dbb001d0fc8487e3f430c90dbb
Artem Kolosovich <soramusoka@gmail.com>
2016-05-27 21:16:39 +0300

    1

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ VERSION="$TRAVIS_BRANCH"-"$TRAVIS_COMMIT"
 ZIP="$VERSION".zip
 DOCKER_CREDENTIALS_FILE=".dockercfg"
 
+cp ~/.docker/config.json .dockercfg
+
 aws configure set default.region "$DOCKER_REPOSITORY_REGION"
 
 sed -i='' "s/<EB_BUCKET_NAME>/$EB_BUCKET_NAME/" Dockerrun.aws.json
@@ -16,7 +18,7 @@ sed -i='' "s/<TRAVIS_BRANCH>/$TRAVIS_BRANCH/" Dockerrun.aws.json
 zip -r "$ZIP" Dockerrun.aws.json
 
 aws s3 cp "$ZIP" s3://"$EB_BUCKET_NAME"/"$ZIP"
-aws s3 cp "$DOCKER_CREDENTIALS_FILE" s3://"$EB_BUCKET_NAME"/"$DOCKER_CREDENTIALS_FILE"
+aws s3 cp "$ZIP" s3://"$EB_BUCKET_NAME"/"$ZIP"
 
 aws elasticbeanstalk create-application-version --application-name "$EB_APP_NAME" --version-label "$VERSION" --source-bundle S3Bucket="$EB_BUCKET_NAME",S3Key="$ZIP" --region "us-west-2"
 aws elasticbeanstalk update-environment --environment-name "$EB_ENV_NAME" --version-label "$VERSION" --region "us-west-2"
